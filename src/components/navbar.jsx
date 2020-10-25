7285e55228b2cdaddfa64dac4b496d13e09914e3
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-10-25 22:53:25 +0100

    functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          Navbar{" "}
-          <span className="badge badge-pill badge-warning">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Navbar
+        <span className="badge badge-pill badge-warning ml-2">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
